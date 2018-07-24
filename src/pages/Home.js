4a818a265426c4aa80323a991342530271446c96
Iván Wolf <ivanwolf15@gmail.com>
2018-07-24 16:26:19 -0400

    add side menu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      openMenu: true,
+      openMenu: false,
       openForm: false,
       location: null,
       places: [],
