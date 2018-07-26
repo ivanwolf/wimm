70e5136dab714d1d3a3a969b2a7d59c44c14d176
Iván Wolf <ivanwolf15@gmail.com>
2018-07-26 14:50:15 -0400

    new colors

--- src/config/colors.js
+++ src/config/colors.js
@@ -6,4 +6,8 @@ export default {
   grayLight: '#c7c7c7',
   grayLighter: '#f5f5f5',
   white: '#ffffff',
+  green: '#5ecd69',
+  yellow: '#ffcb2b',
+  blueLight: '#4285f4',
+  orange: '#f5820b',
 };
